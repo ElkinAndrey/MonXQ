50df2b44ffb4af2b3a4611d1d49b18fc90b4866f
Denis Melnikov <melnikovdv14@gmail.com>
2023-01-11 02:56:09 +0500

    Добавлен базовый скрипт сервера, package.json, .gitignore для api

---
+++ monxq-api/source/server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+const PORT = 5000; //порт, который прослушивает сервер
+const urlConnectDB = 'mongodb+srv://Sinen:MMelnikovB_1403@cluster0.5mbrsrv.mongodb.net/?retryWrites=true&w=majority';
+const app = express();
+app.use(express.json());
+app.use(cors());
+mongoose.set('strictQuery', false);
+startApp();
+
+async function startApp(){
+    try{
+        await mongoose.connect(urlConnectDB, {useUnifiedTopology: true, useNewUrlParser: true}, () => console.log('Data base connection: successfull\n'));
+        app.listen(PORT, () => console.log('Server running on port: ' + PORT));
+    }
+    catch(ex){
+        console.log(ex);
+    }
+}
